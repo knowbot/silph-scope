c4fad24339e8d20eeebbb809472cf9a32195d2e0
knowbot <andre.trevisin@gmail.com>
2026-01-08 20:38:59 +0100

    Set up IMemoryManager interface

---
+++ SilphScope/SilphScope/Models/IMemoryManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SilphScope.Models;
+
+public interface IMemoryManager
+{
+    public static abstract IEnumerable<MemoryRegion> GetMemoryRegions(Process process);
+    public static abstract byte[] ReadMemory(Process process, IntPtr address, int size);
+}
---
+++ SilphScope/SilphScope/Models/MemoryRegion.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SilphScope.Models;
+
+public struct MemoryRegion
+{
+    public IntPtr BaseAddress { get; set; }
+    public int Size { get; set; }
+    public bool IsReadable { get; set; }
+}
