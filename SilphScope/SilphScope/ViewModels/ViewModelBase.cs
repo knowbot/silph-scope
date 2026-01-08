17d0a0fd777ce571f49598609f453850794181c7
knowbot <andre.trevisin@gmail.com>
2026-01-08 19:48:41 +0100

    Added Avalonia base template

---
+++ SilphScope/SilphScope/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SilphScope.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ SilphScope/SilphScope/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SilphScope.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
