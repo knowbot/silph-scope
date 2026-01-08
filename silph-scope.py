4f4a58dd58987ce419989eff52f29cec422834a7
knowbot <andre.trevisin@gmail.com>
2026-01-08 19:44:06 +0100

    Adding test code, JSON schema

---
+++ silph-scope.py
@@ -0,0 +1,56 @@
+import pymem
+import pymem.pattern
+import struct
+import re
+import json
+
+type GameInfo = dict[str, list[str], int, int]
+save_process: str= "DeSmuME_0.9.13_x64.exe"
+game_versions: dict = json.load(open("json/game-data.json"))
+
+def byte_pattern(words: list[str]) -> bytes:
+    byte_array = bytearray()
+    for w in words:
+        byte_array.extend(struct.pack('<I', int(w, 16)))
+    return bytes(byte_array)
+
+def get_addresses(pm: pymem.Pymem, version: str) -> tuple[int, int]:
+    game = game_versions[version]
+    pattern = re.escape(byte_pattern(game["anchor"]))
+    print(f"Scanning process memory for anchor for game version {version}.")
+    res = pymem.pattern.pattern_scan_all(pm.process_handle, pattern, return_multiple=True)
+    if not res:
+        print("Anchor not found. Exiting.")
+        return 0, 0
+    
+    for anchor_addr in res:
+        base_addr = anchor_addr - int(game["anchor_offset"], 16)
+        raw_save_addr = pm.read_int(base_addr + int(game["save_pointer_offset"], 16))
+        if raw_save_addr != 0:
+            print("Save data address found.")
+            save_addr = (raw_save_addr - 0x02000000) + base_addr
+            return base_addr, save_addr
+    print('No valid addresses found. Returning.')
+    return 0, 0
+    
+    
+def get_money(pm: pymem.Pymem, save_addr: int) -> int:
+    return pm.read_uint(save_addr + 0x90)
+
+def get_trainer_id(pm: pymem.Pymem, save_addr: int) -> int:
+    return pm.read_ushort(save_addr + 0x8C)
+
+def get_party_size(pm: pymem.Pymem, save_addr: int) -> int:
+   return int.from_bytes(pm.read_bytes(save_addr + 0xB0, 1), 'little')
+
+
+
+pm = pymem.Pymem(save_process)
+print(f"Attached to {save_process}")
+
+base_addr, save_addr = get_addresses(pm, "CPUE")
+print(get_money(pm, save_addr))
+print(get_trainer_id(pm, save_addr))
+print(get_party_size(pm, save_addr))
+
+
