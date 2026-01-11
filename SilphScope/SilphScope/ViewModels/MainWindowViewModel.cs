c61667e8746c3b8bb26f7b41296eb7ea0b78120b
Riccardo De Zen <riccardodezen98@gmail.com>
2026-01-11 23:17:12 +0100

    Added drop down for process selection

--- SilphScope/SilphScope/ViewModels/MainWindowViewModel.cs
+++ SilphScope/SilphScope/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace SilphScope.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SilphScope.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    #region ShowMore
+
+    [ObservableProperty]
+    private bool _ShowMore = false;
+
+    partial void OnShowMoreChanged(bool value)
+    {
+        RefreshProcesses();
+    }
+
+    #endregion
+
+    #region Processes
+
+    [ObservableProperty]
+    private ObservableCollection<ProcessViewModel> _Processes = [];
+
+    [ObservableProperty]
+    private ProcessViewModel? _SelectedProcess;
+
+    private void RefreshProcesses()
+    {
+        // Filter processes based on MainWindowTitle.
+        Processes = new(Process.GetProcesses().Select(x => new ProcessViewModel(x)).Where(x => !string.IsNullOrEmpty(x.MainWindowTitle) || ShowMore));
+        SelectedProcess = Processes.FirstOrDefault();
+    }
+
+    #endregion Processes
+
+    public MainWindowViewModel()
+    {
+        RefreshProcesses();
+    }
 }
---
+++ SilphScope/SilphScope/ViewModels/ProcessViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+
+namespace SilphScope.ViewModels
+{
+    /// <summary>
+    /// ViewModel representing a summary of a process' info.
+    /// </summary>
+    public partial class ProcessViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string _Name;
+
+        [ObservableProperty]
+        private int _Pid;
+
+        [ObservableProperty]
+        private string _MainWindowTitle;
+
+        public ProcessViewModel(Process process)
+        {
+            Name = process.ProcessName;
+            Pid = process.Id;
+            MainWindowTitle = process.MainWindowTitle;
+        }
+    }
+}
