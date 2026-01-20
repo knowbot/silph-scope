158f46fd6cb070c350d0e1fb892f74afc8f506bb
Riccardo De Zen <riccardodezen98@gmail.com>
2026-01-21 00:02:19 +0100

    Automatically drop log messages after a while

--- SilphScope/ViewModels/LogViewModel.cs
+++ SilphScope/ViewModels/LogViewModel.cs
@@ -1,37 +1,49 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
 using SilphScope.Models.Core;
 using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace SilphScope.ViewModels
 {
-    public partial class LogViewModel : ViewModelBase, IDisposable
-    {
-        [ObservableProperty]
-        private ObservableCollection<string> _Messages = [];
+	public partial class LogViewModel : ViewModelBase, IDisposable
+	{
+		private const int MessageDurationMs = 5000;
 
-        public LogViewModel()
-        {
-            SilphLogger.Message += SilphScopeLogger_Message;
-        }
+		[ObservableProperty]
+		private ObservableCollection<string> _Messages = [];
 
-        private void SilphScopeLogger_Message(object sender, string message)
-        {
-            Messages.Add(message);
-        }
+		public LogViewModel()
+		{
+			SilphLogger.Message += SilphScopeLogger_Message;
+		}
 
-        private bool isDisposed;
+		private void SilphScopeLogger_Message(object sender, string message)
+		{
+			Messages.Add(message);
 
-        public void Dispose()
-        {
-            if (isDisposed)
-            {
-                return;
-            }
+			// Delay and then come back to main thread to remove message.
+			Task.Delay(MessageDurationMs).ContinueWith(_ => Dispatcher.UIThread.Post(() => RemoveMessage(message)));
+		}
 
-            SilphLogger.Message -= SilphScopeLogger_Message;
+		private void RemoveMessage(string message)
+		{
+			Messages.RemoveAt(Messages.IndexOf(message));
+		}
 
-            isDisposed = true;
-        }
-    }
+		private bool isDisposed;
+
+		public void Dispose()
+		{
+			if (isDisposed)
+			{
+				return;
+			}
+
+			SilphLogger.Message -= SilphScopeLogger_Message;
+
+			isDisposed = true;
+		}
+	}
 }
