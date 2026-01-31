e59c3098e039f1d8111e61e6a510a9258d50440c
Riccardo De Zen <riccardodezen98@gmail.com>
2026-01-31 22:58:25 +0100

    Added types enum

---
+++ SilphScope/Models/Games/StaticData/Enums/Types.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace SilphScope.Models.Games.StaticData.Enums
+{
+	public enum Types
+	{
+		Normal,
+		Fighting,
+		Flying,
+		Poison,
+		Ground,
+		Rock,
+		Bird,
+		Bug,
+		Ghost,
+		Steel,
+		[Description("???")]
+		Unknown,
+		Fire,
+		Water,
+		Grass,
+		Electric,
+		Psychic,
+		Ice,
+		Dragon,
+		Dark,
+		Fairy,
+		Stellar
+	}
+}
